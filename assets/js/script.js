3dfac6da4cb12ea71c7f663a429ca422ee336d21
Asa Grace <asag182@gmail.com>
2021-05-18 16:05:58 -0600

    add Taskhandler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,14 @@
-//create variables
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var buttonE1 = document.querySelector("#save-task");
-var tasksToDoE1 = document.querySelector("#tasks-to-do")
-//ad createTaskHandler()
-var createTaskHandler = function() {
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
+var createTaskHandler = function(event) {
+  
+  event.preventDefault();
+  
+  var listItemE1 = document.createElement("li");
+  listItemE1.className = "task-item";
+  listItemE1.textContent = "This is a new task.";
+  tasksToDoE1.appendChild(listItemE1);
+
+  formEl.addEventListener("submit", createTaskHandler);
 }
-
-//add eventListener
-buttonE1.addEventListener("click", createTaskHandler);
-
-buttonE1.addEventListener("click", createTaskHandler); {
-}
-
